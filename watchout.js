f4a63f4615d62a605507c84ac3d6df6c405c6df8
Hack Reactor Students <accounts@hackreactor.com>
2015-04-03 21:07:26 -0700

    Implemented moving enemies

--- watchout.js
+++ watchout.js
@@ -6,103 +6,53 @@ gameOptions.numEnemies = 30;
 
 var enemies = [];
 
+//store enemies into array
 for(var i = 0; i < gameOptions.numEnemies; i++) {
   var enemy = {};
   enemy.id = i;
-  enemy.link = "asteroid.png";
-  enemy.x = Math.floor(gameOptions.width * Math.random());
-  enemy.y = Math.floor(gameOptions.height * Math.random());
-  enemy.height = '20px';
-  enemy.width = '20px';
+  enemy.x = gameOptions.width * Math.random();
+  enemy.y = gameOptions.height * Math.random();
   enemies.push(enemy);
 }
 
 //Set game board
-d3.select('p').append("svg")
+var gameBoard = d3.select('p').append("svg")
 .attr('width', gameOptions.width)
 .attr('height', gameOptions.height);
 
+//initialize myself
+var myself = gameBoard.selectAll('circle.myself')
+  .data([{'id': 999, 'x': gameOptions.width/2, 'y': gameOptions.height/2}],
+    function(d){return d.id;});
 
-var onceAndForAll = function(){
+//add myself to board
+myself.enter()
+  .append('svg:circle')
+  .attr('class', 'myself')
+  .attr('cx', function(d){return d.x;})
+  .attr('cy', function(d){return d.y;})
+  .attr('r', 5)
+  .attr('fill', 'red');
 
-  var currentEnemies = d3.select('svg').selectAll("image").data(enemies);
+//initialize enemies
+var currentEnemies = gameBoard.selectAll('circle.enemy')
+  .data(enemies, function(d){return d.id;});
 
-  //update
-  currentEnemies
-  // .attr('x', function(d){return d.x;})
-  // .attr('y', function(d){return d.y;})
+//add enemies to board
+currentEnemies.enter()
+  .append('svg:circle')
+    .attr('class', 'enemy')
+    .attr('cx', function(d){return d.x;})
+    .attr('cy', function(d){return d.y;})
+    .attr('r', 5);
+
+//update enemies
+var move = function(){
+currentEnemies
   .transition()
   .duration(1000)
-  .attr('x', function(d){return Math.random() * 1000;})
-  // .attr('y', function(d){return Math.random() * 1000;});
-
-  //add
-  currentEnemies
-  .enter()
-  .append("image")
-  .attr('xlink:href', function(d){return d.link;})
-  .attr('x', function(d){return d.x;})
-  .attr('y', function(d){return d.y;})
-  .attr('height', function(d){return d.height;})
-  .attr('width', function(d){return d.width;});
-
-  currentEnemies.transition().duration(1000);
-  console.log("....");
-}
-
-var scramble = function() {
-  _.each(enemies, function(item) {
-    item.x = Math.floor(gameOptions.width * Math.random());
-    item.y = Math.floor(gameOptions.height * Math.random());
-  });
+  .attr('cx', function(d){return Math.random() * gameOptions.width;})
+  .attr('cy', function(d){return Math.random() * gameOptions.height;});
 };
 
-// update.
-var updateEnemies = function() {
-  console.log(enemies[1].x, enemies[1].y);
-  scramble();
-  console.log(enemies[1].x, enemies[1].y);
-  currentEnemies.data(enemies)
-    .append("image")
-    .transition()
-    .attr('x', function(d){return d.x;})
-    .attr('y', function(d){return d.y;})
-    .duration(1000);
-    console.log('hey vince');
-};
-
-// add.
-var addEnemies = function(enemies) {
-  currentEnemies.data(enemies)
-    .enter()
-    .append("image")
-    .attr('xlink:href', function(d){return d.link;})
-    .attr('x', function(d){return d.x;})
-    .attr('y', function(d){return d.y;})
-    .attr('height', function(d){return d.height;})
-    .attr('width', function(d){return d.width;});
-};
-
-// delete.
-var exterminate = function(enemies) {
-  currentEnemies.data(enemies)
-  .exit()
-  .remove();
-};
-
-// addEnemies(enemies);
-// updateEnemies(enemies);
-setInterval(function() {
-  onceAndForAll();
-}, 1500);
-
-//Add enemies to board
-// d3.select('svg').selectAll("image")
-// .data(enemies)
-// .enter()
-// .append("image")
-// .attr('xlink:href', function(d){return d.link;})
-// .attr('x', function(d){return d.x;})
-// .attr('y', function(d){return d.y;})
-// .attr('height', function(d){return d.height;})
-// .attr('width', function(d){return d.width;});
+setInterval(function(){move()}, 1500);
