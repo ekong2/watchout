52c33ceff6edb157f5d918bdd006fdff694ef8d9
Hack Reactor Students <accounts@hackreactor.com>
2015-04-04 11:38:35 -0700

    Implemented collision tracking

--- watchout.js
+++ watchout.js
@@ -1,17 +1,42 @@
-// start slingin' some d3 here.
+//Initialize game options
 var gameOptions = {};
 gameOptions.height = 450;
 gameOptions.width = 700;
 gameOptions.numEnemies = 30;
+gameOptions.highScore = 0;
+gameOptions.score = 0;
+gameOptions.collisions = 0;
 
+//initialize myself obj
+var myself = {};
+myself.id = 999;
+myself.x = gameOptions.width * .5;
+myself.y = gameOptions.height * .5;
+myself.r = 5;
+
+var highScoreSelection = d3.select('.high').select('span');
+var currentScore = d3.select('.current').select('span');
+var collisions = d3.select('.collisions').select('span');
+
+//create drag property
+var drag = d3.behavior.drag()
+    .on("drag", function(d) {
+        d.x += d3.event.dx;
+        d.y += d3.event.dy;
+        d3.select(this).attr("transform", function(d,i){
+            return "translate(" + [ d.x,d.y ] + ")";
+        });
+    });
+
+//initialize enemies array
 var enemies = [];
 
-//store enemies into array
 for(var i = 0; i < gameOptions.numEnemies; i++) {
   var enemy = {};
   enemy.id = i;
   enemy.x = gameOptions.width * Math.random();
   enemy.y = gameOptions.height * Math.random();
+  enemy.r = 5;
   enemies.push(enemy);
 }
 
@@ -20,21 +45,21 @@ var gameBoard = d3.select('p').append("svg")
 .attr('width', gameOptions.width)
 .attr('height', gameOptions.height);
 
-//initialize myself
-var myself = gameBoard.selectAll('circle.myself')
-  .data([{'id': 999, 'x': gameOptions.width/2, 'y': gameOptions.height/2}],
+//initialize myself selection
+var self = gameBoard.selectAll('circle.myself')
+  .data([myself],
     function(d){return d.id;});
 
-//add myself to board
-myself.enter()
+//add self to board
+self.enter()
   .append('svg:circle')
   .attr('class', 'myself')
-  .attr('cx', function(d){return d.x;})
-  .attr('cy', function(d){return d.y;})
+  .attr("transform", function(d) { return "translate(" + [d.x,d.y] + ")"})
   .attr('r', 5)
-  .attr('fill', 'red');
+  .attr('fill', 'red')
+  .call(drag);
 
-//initialize enemies
+//initialize enemies selection
 var currentEnemies = gameBoard.selectAll('circle.enemy')
   .data(enemies, function(d){return d.id;});
 
@@ -44,15 +69,45 @@ currentEnemies.enter()
     .attr('class', 'enemy')
     .attr('cx', function(d){return d.x;})
     .attr('cy', function(d){return d.y;})
-    .attr('r', 5);
+    .attr('r', function(d){return d.r;});
+
+//create checkCollisions
+var checkCollisions = function(enemy, updateScore) {
+  var radiusSum = enemy.attr('r') + myself.r;
+  var xDiff = enemy.attr('cx') - myself.x;
+  var yDiff = enemy.attr('cy') - myself.y;
+
+  var separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff,2));
+  if (separation < radiusSum){
+    updateScore();
+  }
+};
+
+var tweenWithCollisions = function(){
+  var enemy = d3.select(this);
+  return (checkCollisions(enemy, updateScore));
+};
+
+//create updateScore
+var updateScore = function() {
+  if(gameOptions.highScore < gameOptions.score){
+    gameOptions.highScore = gameOptions.score;
+    gameOptions.score = 0;
+    highScoreSelection.text(gameOptions.highScore);
+  }
+  gameOptions.collisions++;
+  collisions.text(gameOptions.collisions);
+};
 
 //update enemies
 var move = function(){
 currentEnemies
   .transition()
   .duration(1000)
+  .tween('custom', tweenWithCollisions)
   .attr('cx', function(d){return Math.random() * gameOptions.width;})
   .attr('cy', function(d){return Math.random() * gameOptions.height;});
 };
+
 
 setInterval(function(){move()}, 1500);
